9d10bedbccd8a4417754ec9cd5fd267c1de326ca
Uday <admin@udayteja.com>
2023-10-19 16:15:01 +0530

    Add Builder Class

---
+++ Builder/src/Builder.java
@@ -0,0 +1,77 @@
+//import javax.xml.stream.events.DTD;
+//
+//public class Builder {
+//
+//    int id;
+//    String name;
+//    double psp;
+//    int age;
+//    String univName;
+//    int gradYear;
+//
+////    Student(int id, String name...) {}
+//
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public Builder setId(int id) {
+//        this.id = id;
+//        return this;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public Builder setName(String name) {
+//        this.name = name;
+//        return this;
+//    }
+//
+//    public double getPsp() {
+//        return psp;
+//    }
+//
+//    public Builder setPsp(double psp) {
+//        this.psp = psp;
+//        return this;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public Builder setAge(int age) {
+//        this.age = age;
+//        return this;
+//    }
+//
+//    public String getUnivName() {
+//        return univName;
+//    }
+//
+//    public Builder setUnivName(String univName) {
+//        this.univName = univName;
+//        return this;
+//    }
+//
+//    public int getGradYear() {
+//        return gradYear;
+//    }
+//
+//    public Builder setGradYear(int gradYear) {
+//        this.gradYear = gradYear;
+//        return this;
+//    }
+//
+//    public Student build() {
+//
+//        if (this.getAge() > 25) {
+//            throw new IllegalArgumentException();
+//        }
+//
+//        return new Student(this);
+//    }
+//}
