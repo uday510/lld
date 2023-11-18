f314842739771d6e55e432be589d02de99896e46
Uday <budayteja009@gmail.com>
2023-11-18 16:40:08 +0530

    Add Observer Pattern

---
+++ Observer/src/WeatherData.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject {
+    private List<Observer> observers;
+    private float temperature, humidity, pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<>();
+    }
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+    public void measurementsChanged() {
+        notifyObservers();;
+    }
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+}
