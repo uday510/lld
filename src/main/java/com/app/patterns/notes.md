f077cd701f56a4be3d450ef8a7b0197ade3af415
Uday <budayteja009@gmail.com>
2025-04-19 11:10:46 +0530

    Add SingleResponsibility Example

---
+++ src/main/java/com/app/patterns/notes.md
@@ -0,0 +1,11 @@
+Pattern name: This is an identification step that describes a design problem.
+
+Intent: This step describes the use case of the particular design pattern.
+
+Motivation: This step illustrates the problem and talks about the inner components in the pattern to solve the problem.
+
+Structure: This step visualizes the structure using a graphical representation of the classes in the pattern.
+
+Consequences: This step describes the trade-offs of a particular pattern.
+
+Implementation: This step illustrates an example of code in any popular programming language.
---
+++ src/main/java/com/app/principles/SingleResponsibility.java
@@ -0,0 +1,48 @@
+package com.app.principles;
+
+
+class Employee {
+    private String name;
+    private String position;
+
+    public Employee(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
+
+class EmployeeRepository {
+    public void save(Employee employee) {
+        System.out.println("Saving " + employee.getName() + " to the database...");
+    }
+}
+
+class EmployeeReport {
+    public void printReport(Employee employee) {
+        System.out.println("Employee Report");
+        System.out.println("---------------");
+        System.out.println("Name: " + employee.getName());
+        System.out.println("Position: " + employee.getPosition());
+    }
+}
+
+public class SingleResponsibility {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("John Doe", "Fake God");
+
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+        employeeRepository.save(employee);
+
+        EmployeeReport employeeReport = new EmployeeReport();
+        employeeReport.printReport(employee);
+    }
+}
