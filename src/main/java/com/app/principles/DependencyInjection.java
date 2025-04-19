3389863c89ec95461f289d0a251296ce40117863
Uday <budayteja009@gmail.com>
2025-04-19 11:53:29 +0530

    Add DependencyInjection Example

---
+++ src/main/java/com/app/principles/DependencyInjection.java
@@ -0,0 +1,27 @@
+package com.app.principles;
+
+/*
+Dependency Injection is a design pattern in which an object receives its dependencies from an external source rather than creating them itself.
+ */
+
+class NotificationService {
+    private MessageService messageService;
+
+    // Constructor injection
+    public NotificationService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    public void notifyUser(String msg) {
+        messageService.sendMessage(msg);
+    }
+}
+
+public class DependencyInjection {
+    public static void main(String[] args) {
+        MessageService email = new EmailService(); // low-level module
+        NotificationService service = new NotificationService(email); // injected here
+
+        service.notifyUser("Hello! Welcome to SOLID principles.");
+    }
+}
